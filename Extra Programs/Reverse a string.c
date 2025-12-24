57548794cfb40c2a59f57578816dcc16b696b806
Ananya Ranjith <ananyaranjith92@gmail.com>
2025-12-24 16:37:20 +0530

    Add files via upload

---
+++ Extra Programs/Reverse a string.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char ch) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        stack[++top] = ch;
+    }
+}
+
+char pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return '\0';
+    } else {
+        return stack[top--];
+    }
+}
+
+int main() {
+    char str[MAX];
+    int i, len;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    len = strlen(str);
+
+
+    for (i = 0; i < len; i++) {
+        push(str[i]);
+    }
+
+
+    for (i = 0; i < len; i++) {
+        str[i] = pop();
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
+
